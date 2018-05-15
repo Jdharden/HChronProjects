b08bf6b8a906587a0232f493e920ddca25ff8716
John D. Harden <Jdharden123@gmail.com>
2018-05-15 18:57:17 -0500

    Update function documentation

--- R Scripts/Harvey/HarveyAnalysis.R
+++ R Scripts/Harvey/HarveyAnalysis.R
@@ -1,10 +1,29 @@
+# High Water Rescues Analysis.R
+# By John D. Harden at the Houston Chronicle
+
+#####################################################################################################################################
+
+#######################################################################################################################################
+
+library (tidyverse)
+
+setwd("~/Documents/Harvey911")
+
 911 call anaylsis <- function () {
 read.csv ("Harvey.csv", header=TRUE)
-
+summary(HarveyDataPlots)
 mydata = read.csv("Harvey.csv") 
-
 stripchart(Hours, method = "jitter", 
 pch = 20, 
 xlab = "No. of Hours")
 
 } 
+
+summary(HarveyDataPlots)
+
+function(Harvey27) {
+HurricaneByDay <- filter(HarveyDataPlots, Day == 27) 
+ggplot(HurricaneByDay, aes(x=test_X, y=test_Y, color = Hour), alpha = 1/3) + 
+geom_point()
+
+}
